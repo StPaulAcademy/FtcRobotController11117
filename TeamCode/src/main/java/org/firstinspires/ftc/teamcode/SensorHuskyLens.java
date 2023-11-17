bdf9caddfe094bf095259eff3ee1dd7b494b9c58
26annaliseva <26annaliseva@students.spa.edu>
2023-11-17 17:25:46 -0600

    husky lens basic code - team scoring element

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorHuskyLens.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+@TeleOp(name = "Sensor: HuskyLens", group = "Sensor")
+@Disabled
+
+public class SensorHuskyLens extends LinearOpMode {
+
+    private final int READ_PERIOD = 1;
+
+
+    public void runOpMode() {
+        HuskyLens huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
+        Deadline rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
+        rateLimit.expire();
+
+        /*
+         * Basic check to see if the device is alive and communicating.  This is not
+         * technically necessary here as the HuskyLens class does this in its
+         * doInitialization() method which is called when the device is pulled out of
+         * the hardware map.  However, sometimes it's unclear why a device reports as
+         * failing on initialization.  In the case of this device, it's because the
+         * call to knock() failed.
+         */
+        if (!huskyLens.knock()) {
+            telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
+        } else {
+            telemetry.addData(">>", "Press start to continue");
+        }
+
+        /*
+         * The device uses the concept of an algorithm to determine what types of
+         * objects it will look for and/or what mode it is in.  The algorithm may be
+         * selected using the scroll wheel on the device, or via software as shown in
+         * the call to selectAlgorithm().
+         *
+         * The SDK itself does not assume that the user wants a particular algorithm on
+         * startup, and hence does not set an algorithm.
+         *
+         * Users, should, in general, explicitly choose the algorithm they want to use
+         * within the OpMode by calling selectAlgorithm() and passing it one of the values
+         * found in the enumeration HuskyLens.Algorithm.
+         */
+        huskyLens.selectAlgorithm(HuskyLens.Algorithm.OBJECT_CLASSIFICATION);
+
+        telemetry.update();
+        waitForStart();
+
+        /*
+         * Looking for AprilTags per the call to selectAlgorithm() above.  A handy grid
+         * for testing may be found at https://wiki.dfrobot.com/HUSKYLENS_V1.0_SKU_SEN0305_SEN0336#target_20.
+         *
+         * Note again that the device only recognizes the 36h11 family of tags out of the box.
+         */
+        while (opModeIsActive()) {
+            if (!rateLimit.hasExpired()) {
+                continue;
+            }
+            rateLimit.reset();
+
+            /*
+             * All algorithms, except for LINE_TRACKING, return a list of Blocks where a
+             * Block represents the outline of a recognized object along with its ID number.
+             * ID numbers allow you to identify what the device saw.  See the HuskyLens documentation
+             * referenced in the header comment above for more information on IDs and how to
+             * assign them to objects.
+             *
+             * Returns an empty array if no objects are seen.
+             */
+            HuskyLens.Block[] blocks = huskyLens.blocks();
+            telemetry.addData("Block count", blocks.length);
+            for (int i = 0; i < blocks.length; i++) {
+                telemetry.addData("Block", blocks[i].x);
+                //*if x < 100 = 1. If x = 100-200 = 2. If x > 200.
+            }
+
+            telemetry.update();
+        }
+    }
+}
